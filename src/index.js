93b3961ea930accd190d2a21732f80d9397d7e04
Aziz Isapov <zizzxiii@gmail.com>
2021-10-26 20:20:44 +0600

    finished feedback slider

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import "./js/common.js";
 import "./js/slider.js";
+import "./js/feedback-slider.js";
 import "./assets/css/main.css";
 import "./assets/scss/main.scss";
-
--- src/js/feedback-slider.js
+++ src/js/feedback-slider.js
@@ -1,22 +1,46 @@
 import {tns} from "/node_modules/tiny-slider/src/tiny-slider";
 
-const slider = tns({
+const prevButton = document.querySelector(".prev-button"),
+  nextButton = document.querySelector(".next-button"),
+  sliderWrapper = document.querySelector("#feedbackSlider");
+// feedbackStars = document.querySelectorAll(".feedback__star");
+
+const feedbackSlider = tns({
   container: "#feedbackSlider",
   items: 3,
+  slideBy: 1,
+  touch: true,
+  animateIn: "tns-",
+  controls: true,
+  autoHeight: false,
+  nav: true,
+  arrowKeys: true,
+  controlsContainer: "#navContainer",
+  prevButton: "#prevButton",
+  nextButton: "#nextButton",
+  hasControls: true,
+  controlsPosition: "bottom",
   responsive: {
-    480: {
-      items: 1,
+    1024: {
+      items: 3
     },
-    640: {
-      edgePadding: 20,
-      gutter: 20,
-      items: 2
+    992: {
+      items: 2,
     },
-    700: {
-      gutter: 30
+    840: {
+      items: 2,
     },
-    900: {
-      items: 2
+    375: {
+      items: 1
     }
   }
 });
+
+const sliderHeight = +getComputedStyle(sliderWrapper).height.slice(-0, -2);
+
+function setPositionForBtn(btnName) {
+  btnName.style.cssText = `top: ${Math.floor(sliderHeight) / 2}px`;
+}
+
+setPositionForBtn(prevButton);
+setPositionForBtn(nextButton);
