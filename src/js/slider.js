8e9f657c159b85fb323d1d60816ab3df156fc424
Aziz <zizzxiii@gmail.com>
2021-10-27 15:36:48 +0600

    finished project

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,9 +1,9 @@
-import {tns} from "/node_modules/tiny-slider/src/tiny-slider";
+import { tns } from "/node_modules/tiny-slider/src/tiny-slider";
 
-const dots = document.querySelectorAll(".dot");
+const dots = document.querySelectorAll( ".dots .dot" );
 let index = 0;
 
-const slider = tns({
+const slider = tns( {
   container: "#mySlider",
   items: 1,
   slideBy: "page",
@@ -12,36 +12,50 @@ const slider = tns({
   "swipeAngle": false,
   "mode": "gallery",
   nav: dots
-});
+} );
 
-const addActiveClass = (elem, activeClass) => {
-  elem.classList.add(activeClass);
+const sliderBottom = tns( {
+  container: "#mySlider2",
+  items: 1,
+  slideBy: "page",
+  controls: false,
+  speed: 500,
+  swipeAngle: false,
+  mode: "carousel",
+  navContainer: ".dots-2",
+  autoHeight: true,
+  nav: dots,
+} );
+
+const addActiveClass = ( elem, activeClass ) => {
+  elem.classList.add( activeClass );
 };
-const removeActiveClass = (elem, activeClass) => {
-  elem.forEach(d => d.classList.remove(activeClass));
+const removeActiveClass = ( elem, activeClass ) => {
+  elem.forEach( d => d.classList.remove( activeClass ) );
 };
 
-const changeActiveClass = (elem, className, elemArr) => {
-  removeActiveClass(elemArr, className);
-  addActiveClass(elem, className);
+const changeActiveClass = ( elem, className, elemArr ) => {
+  removeActiveClass( elemArr, className );
+  addActiveClass( elem, className );
 };
 
-[...dots].forEach((dot, i) => {
-  dot.addEventListener("click", function (e) {
+[ ...dots ].forEach( ( dot, i ) => {
+  dot.addEventListener( "click", function ( e ) {
     const target = e.target;
-    changeActiveClass(target, "active", dots);
-
-    slider.goTo(i);
+    changeActiveClass( target, "active", dots );
+		
+    slider.goTo( i );
+    sliderBottom.goTo( i );
     index = i;
-  });
-});
+  } );
+} );
 
-setInterval(() => {
-  if (index >= dots.length) {
+setInterval( () => {
+  if ( index >= dots.length ) {
     index = 0;
   }
-  slider.goTo(index);
-  changeActiveClass(dots[index], "active", dots);
+  slider.goTo( index );
+  sliderBottom.goTo( index );
+  changeActiveClass( dots[ index ], "active", dots );
   index++;
-}, 3000);
-
+}, 300000 );
